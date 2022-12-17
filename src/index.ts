add5a405353a8ad7760079251c41a34cf9f691c3
under-sin <andersonvieira818@yahoo.com.br>
2022-12-17 17:21:36 -0300

    configurações iniciais do express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8001;
+
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
